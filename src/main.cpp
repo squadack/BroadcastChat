84aa440a14e47efb151e2012ffd9cbc4df9a57d2
Piotr Majcherczyk <fynxor@gmail.com>
2017-07-04 18:55:13 +0200

    Add cmd line arguments - currently not doing anything.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,39 @@
 #include <QtCore>
 #include "ChatWindow.h"
 
+
 int main(int argc, char **argv)
 {
 	qsrand(time(nullptr));
 	QApplication app{argc, argv};
 	QCoreApplication::setOrganizationName("Squadack");
 	QCoreApplication::setApplicationName("Broadcast Chat");
+
+	QCommandLineParser parser;
+	parser.setApplicationDescription("Broadcast Chat");
+	parser.addHelpOption();
+
+	QCommandLineOption optionShowTime{
+		QStringList{"t", "time"},
+		QObject::tr("Shows time.")
+	};
+	parser.addOption(optionShowTime);
+
+	QCommandLineOption optionShowColours{
+		QStringList{"c", "colours"},
+		QObject::tr("Shows colours.")
+	};
+	parser.addOption(optionShowColours);
+
+	parser.process(app);
+
+	if (parser.isSet(optionShowTime))
+		qDebug() << "option show time: SET!";
+	if (parser.isSet(optionShowColours))
+		qDebug() << "option show colours: SET!";
+
 	ChatWindow window{nullptr};
 	window.show();
-	
+
 	return app.exec();
 }
